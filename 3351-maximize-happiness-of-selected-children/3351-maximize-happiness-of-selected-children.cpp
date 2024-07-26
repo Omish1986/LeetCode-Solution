107eb6e581b712fb5cb52c9b717bbdea3878c132
Omish Pipaliya <121420836+Omish1986@users.noreply.github.com>
2024-07-26 13:39:08 +0530

    Time: 106 ms (96.65%), Space: 107.8 MB (30.01%) - LeetHub

--- 3351-maximize-happiness-of-selected-children/3351-maximize-happiness-of-selected-children.cpp
+++ 3351-maximize-happiness-of-selected-children/3351-maximize-happiness-of-selected-children.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
+    #define mod 1000000007;
     long long maximumHappinessSum(vector<int>& h, int k) {
-        long long int a=0;
-        sort(h.rbegin(),h.rend());
-        for(int i=0;i<k;i++)
-        {
-            int num=h[i]-i;
-            if(num>0)
-            a+=num;
-            else
-            break;
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        if(h.size()==1 && k==1) return h[0]; // if only one child is exist
+        int n=h.size();
+        sort(h.begin(),h.end());// most happiest will be settled at last
+        long long sum=h[n-1];int count=1;
+        for(int i=n-2;k>1;i--){ // iterating from the end of sorted array
+            int num=h[i]-count;
+            if(num<0) break; // if happiness is negative, Then next will be obviously negative
+            sum+=num%mod;
+            k--;count++;
         }
-        return a;
-        
+        return sum;
     }
 };
