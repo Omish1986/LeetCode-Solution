8ca9a9ca2be60bf5e367876ab772362bc2dc2371
Omish Pipaliya <121420836+Omish1986@users.noreply.github.com>
2024-08-05 15:06:56 +0530

    Time: 94 ms (95.08%), Space: 131.7 MB (60.49%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& n, int p) {
+         std::ios_base::sync_with_stdio(false);
+	    std::cin.tie(nullptr);
         vector<int> v;
         int cnt=0;
         for(int i=0;i<n.size();i++)
