02eb8f4c00d8fa28253aa64a16b739ca6c75eba5
Omish Pipaliya <121420836+Omish1986@users.noreply.github.com>
2024-09-01 10:25:55 +0530

    Time: 91 ms (24.59%), Space: 91.5 MB (47.66%) - LeetHub

---
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int>& o, int m, int n) {
+        vector<vector<int>> v;
+        vector<int> vec;
+        int num = o.size();
+        if(num!=m*n)
+        {
+            return v;
+        }
+        for(int i=0;i<num;i++)
+        {
+            if(i%n==0&&i!=0)
+            {
+                v.push_back(vec);
+                vec.clear();
+                vec.push_back(o[i]);
+                cout<<o[i]<<endl;
+            }
+            else
+            {
+                vec.push_back(o[i]);
+            }
+        }
+        v.push_back(vec);
+
+
+
+        return v;
+        
+    }
+};
